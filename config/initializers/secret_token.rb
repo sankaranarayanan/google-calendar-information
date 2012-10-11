8114ae7905c02a0012337d3e2fb3de355da1295d
sankaranarayanan <esankaranarayanan@gmail.com>
2012-10-12 01:09:25 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoogleCalendarInformation::Application.config.secret_token = 'e506f936a065643b3f4ce3aa46d12892f98cbb72b46de846872714e7e8522d8609c7237d4666425dedf983cb3d5046039f64af1e555bab229c8bec6d255bca43'
